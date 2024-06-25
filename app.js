2bb7f2ae5bd3812537910c62fdf683d62051b9e7
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-25 16:05:48 +0200

    updated index.html, styles.css, app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const iconBtns = document.querySelectorAll('.icon');
+const answers = document.querySelectorAll('p');
+const buttons = document.querySelectorAll('button');
+const bg = document.querySelector('.bg');
+let isHidden;
+
+
+if (window.innerWidth > 500) {
+    bg.src = "./assets/images/background-pattern-desktop.svg"
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      handleFAQ(btn)
+    })
+
+    btn.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleFAQ(btn)
+            // console.log('hello')
+        }           
+    })
+})
+
+function handleFAQ(btn) {
+    changeIcon(btn);
+    if (isHidden) {
+        showAnswer(btn);
+    } else {
+        hideAnswer(btn);
+    }
+}  
+
+function changeIcon(btn) {
+    if (btn.children[0].getAttribute('src') === "./assets/images/icon-minus.svg") {
+        btn.children[0].src = "./assets/images/icon-plus.svg";
+        isHidden = false;
+    } else {
+        btn.children[0].src = "./assets/images/icon-minus.svg";
+        isHidden = true;
+    }
+}
+
+function hideAnswer(btn) {
+    answers[Array.from(iconBtns).indexOf(btn.children[0])].classList.add('hidden');
+}
+
+function showAnswer(btn) {
+    answers[Array.from(iconBtns).indexOf(btn.children[0])].classList.remove('hidden')
+}
